e02c8760aa584c658e7593e42e1d531d35c1fbd1
Jasmina Judith <81411489+JasminaJudith@users.noreply.github.com>
2023-02-04 23:57:17 +0100

    desktop version
    add modification to the mobile because i add some div

--- main.js
+++ main.js
@@ -1,51 +1,49 @@
-"use strict";
-
 function carousel() {
   let carouselSlider = document.querySelector(".carousel__slider");
   let list = document.querySelector(".carousel__list");
   let item = document.querySelectorAll(".carousel__item");
   let list2;
   const speed = 1;
-  
+
   const width = list.offsetWidth;
   let x = 0;
   let x2 = width;
-  
+
   console.log(x2);
   function clone() {
-    list2 = list.cloneNode(true);
-    carouselSlider.appendChild(list2);
-    list2.style.left = `${width}px`;
+      list2 = list.cloneNode(true);
+      carouselSlider.appendChild(list2);
+      list2.style.left = `${width}px`;
   }
 
   function moveFirst() {
-    x -= speed;
+      x -= speed;
 
-    if (width >= Math.abs(x)) {
-      list.style.left = `${x}px`;
-    } else {
-      x = width;
-    }
+      if (width >= Math.abs(x)) {
+          list.style.left = `${x}px`;
+      } else {
+          x = width;
+      }
   }
 
   function moveSecond() {
-    x2 -= speed;
+      x2 -= speed;
 
-    if (list2.offsetWidth >= Math.abs(x2)) {
-      list2.style.left = `${x2}px`;
-    } else {
-      x2 = width;
-    }
+      if (list2.offsetWidth >= Math.abs(x2)) {
+          list2.style.left = `${x2}px`;
+      } else {
+          x2 = width;
+      }
   }
 
   function hover() {
-    clearInterval(a);
-    clearInterval(b);
+      clearInterval(a);
+      clearInterval(b);
   }
 
   function unhover() {
-    a = setInterval(moveFirst, 10);
-    b = setInterval(moveSecond, 10);
+      a = setInterval(moveFirst, 10);
+      b = setInterval(moveSecond, 10);
   }
 
   clone();
